b79205588730151710112ccdb601313c688a5bb7
Athena Chang <athenachang17@gmail.com>
2026-02-12 11:52:54 -0500

    Time: 0 ms (100%), Space: 76.6 MB (63.13%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var findCenter = function(edges) {
-    const seen = {}
+    const seen = new Set()
 
     for (const [node1, node2] of edges) {
-        if (seen[node1]) {
+        if (seen.has(node1)) {
             return node1
         }
-        if (seen[node2]) {
+        if (seen.has(node2)) {
             return node2
         }
-        seen[node1] = true
-        seen[node2] = true
+        seen.add(node1)
+        seen.add(node2)
     }
 };
